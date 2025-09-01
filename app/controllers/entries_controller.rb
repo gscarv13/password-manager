8c8f8b315a18323394685be2a24ef1cb39306bc1
Gustavo Carvalho <gsc13@tuta.io>
2025-09-01 19:22:37 -0300

    feat: add entries

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,25 @@
+class EntriesController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @entry = Entry.new
+  end
+
+  def create
+    @entry = current_user.entries.new(entry_params)
+
+    if @entry.save
+      flash[:notice] = "Entry has been saved"
+      redirect_to root_path
+    else
+      flash[:alert] = "An issue happened while saving the entry"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def entry_params
+    params.expect(entry: [ :name, :url, :username, :password ])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
   get "/home", to: "pages#home"
   get "/about", to: "pages#about"
+
+  resources :entries
+
   # Defines the root path route ("/")
   root "pages#home"
 end
