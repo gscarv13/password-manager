f18e7fc55cfdaec131425d7c9f1f3193ab0794e8
Gustavo Carvalho <gsc13@tuta.io>
2025-09-02 23:51:37 -0300

    feat: add edit and destroy actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+user = User.first
+
+Entry.create!(
+  user:,
+  name: "Google",
+  url: "https://google.com",
+  username: "google_user@gmail.com",
+  password: "google_password"
+)
+
+Entry.create!(
+  user:,
+  name: "Facebook",
+  url: "https://facebook.com",
+  username: "facebook_user",
+  password: "facebook_password"
+)
+
+Entry.create!(
+  user:,
+  name: "Twitter",
+  url: "https://twitter.com",
+  username: "twitter_user",
+  password: "twitter_password"
+)
+
+Entry.create!(
+  user:,
+  name: "Amazon",
+  url: "https://amazon.com",
+  username: "amazon_user",
+  password: "amazon_password"
+)
+
+Entry.create!(
+  user:,
+  name: "Apple",
+  url: "https://apple.com",
+  username: "apple_user",
+  password: "apple_password"
+)
